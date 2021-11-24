e6eadd3ef7176a464620f082f333044dbb7cf44a
Kostas Skenderidis <skenderidis@gmail.com>
2021-11-24 17:38:48 +0400

    Update locust.py

--- locust.py
+++ locust.py
@@ -7,22 +7,3 @@ class HackazonUser(HttpUser):
     @task(2)
     def main_page(self):
         self.client.get("/", verify=False)
-/*
-    @task
-    def view_items(self):
-        self.client.get("/product/view?id=72", verify=False)
-        self.client.get("/product/view?id=81", verify=False)
-        self.client.get("/product/view?id=16", verify=False)
-        self.client.get("/category/view?id=2", verify=False)
-        self.client.get("/category/view?id=3", verify=False)
-        self.client.get("/category/view?id=1", verify=False)
-
-
-
-
-    @task(2)
-    def other_tasks(self):
-        self.client.get("/faq", verify=False)
-        self.client.get("/contact", verify=False)
-        self.client.get("/", verify=False)
-*/
